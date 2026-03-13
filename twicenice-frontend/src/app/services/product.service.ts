8e694c3bfd2b21f04d4538ecb583be6ddc0f02c4
Ashika K <ashika120110@gmail.com>
2026-03-14 01:14:46 +0530

    Update product.service.ts

--- twicenice-frontend/src/app/services/product.service.ts
+++ twicenice-frontend/src/app/services/product.service.ts
@@ -5,7 +5,7 @@ import { Product } from '../models/product.model';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
-  private baseUrl = 'http://localhost:8080/api/products';
+   private baseUrl = 'https://twicenice-backend-docker.onrender.com/api/products';
 
   constructor(private http: HttpClient) {}
 
