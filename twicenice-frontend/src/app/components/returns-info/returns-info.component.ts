3ebf5a0ed70e54945669aabbaec6980ea112f3c3
Ashika K <ashika120110@gmail.com>
2026-03-28 16:51:40 +0530

    Create returns-info.component.ts

---
+++ twicenice-frontend/src/app/components/returns-info/returns-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-returns-info',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './returns-info.component.html'
+})
+export class ReturnsInfoComponent {}
