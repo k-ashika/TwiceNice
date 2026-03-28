fdfd124a6b9549c6476bbe2a6d7486902cee9d44
Ashika K <ashika120110@gmail.com>
2026-03-28 16:40:02 +0530

    Update footer.component.ts

--- twicenice-frontend/src/app/layout/footer/footer.component.ts
+++ twicenice-frontend/src/app/layout/footer/footer.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule, MatListModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
