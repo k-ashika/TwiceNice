b92b9c07054b5be93e96a2cd869c5ce0712cabba
Ashika K <ashika120110@gmail.com>
2026-03-28 17:10:44 +0530

    Update app.config.ts

--- twicenice-frontend/src/app/app.config.ts
+++ twicenice-frontend/src/app/app.config.ts
@@ -1,17 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
-import { provideAnimations } from '@angular/platform-browser/animations'; // <-- Add this import
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled'
+      })
+    ),
     provideHttpClient(withInterceptorsFromDi()),
-    provideAnimations(), // <-- Add this provider
+    provideAnimations(),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
